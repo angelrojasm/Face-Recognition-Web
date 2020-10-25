627145aa93677101eb05af77d51c218c7eb6402c
angelrojasm <angelrojasm6@gmail.com>
2020-10-24 21:49:08 -0400

    Added AWS S3 controller module

---
+++ packages/server/aws/config/config.js
@@ -0,0 +1,11 @@
+(accessKeyID = ''),
+	(secretKey = ''),
+	(bucket = ''),
+	(region = '');
+
+module.exports = {
+	accessKeyID,
+	secretKey,
+	bucket,
+	region,
+};
---
+++ packages/server/aws/controller/s3.js
@@ -0,0 +1,50 @@
+const aws = require('aws-sdk');
+const config = require('../config/config');
+const s3Bucket = config.bucket;
+aws.config.setPromisesDependency();
+aws.config.update({
+	region: config.region,
+	accessKeyId: config.accessKeyID,
+	secretAccessKey: config.secretKey,
+});
+
+const s3 = new aws.S3();
+
+exports.uploadFile = (file, fileName) => {
+	return new Promise((resolve, reject) => {
+		let fileParts = file.name.split('.');
+		let fileType = fileParts[1];
+
+		s3.putObject(
+			{
+				Bucket: s3Bucket,
+				Key: fileName,
+				ACL: 'public-read',
+				Body: file.data,
+				Metadata: { type: fileType },
+			},
+			function (err) {
+				if (err) {
+					return reject(err);
+				} else {
+					return resolve('Ok');
+				}
+			}
+		);
+	});
+};
+exports.getFile = (fileName, res) => {
+	s3.getObject(
+		{
+			Bucket: s3Bucket,
+			Key: fileName,
+		},
+		(err, data) => {
+			if (err) {
+				res.send('No such file name found.');
+			} else {
+				res.send(data.Body);
+			}
+		}
+	);
+};
