2bd6d9e758f98832b652e2530bc7dd9a81adc02c
herrand15 <20165239@ce.pucmm.edu.do>
2020-10-25 23:37:55 -0400

    adding image to collection

--- packages/server/aws/config/config.js
+++ packages/server/aws/config/config.js
@@ -1,7 +1,7 @@
-(accessKeyID = ''),
-	(secretKey = ''),
-	(bucket = ''),
-	(region = '');
+(accessKeyID = 'AKIAT245WJKZFLLK3AJ7'),
+	(secretKey = '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'),
+	(bucket = 'missing-people'),
+	(region = 'us-east-1');
 
 module.exports = {
 	accessKeyID,
